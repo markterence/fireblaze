54f842e257958580baf75490c0f5d32363eebc29
Mark Terence <mtiglao.ccheetah@gmail.com>
2020-04-29 12:25:01 +0800

    create authentication vuex store and nuxt middleware

---
+++ store/auth.js
@@ -0,0 +1,30 @@
+import axios from 'axios'
+
+export const state = () => ({
+  authUser: null
+})
+
+export const mutations = {
+  SET_USER(state, user) {
+    state.authUser = user
+  }
+}
+
+export const actions = {
+  nuxtServerInit({ commit }, { req }) {
+    if (req.session && req.session.authUser) {
+      commit('SET_USER', req.session.authUser)
+    }
+  },
+  async login({ commit }, { username, password }) {
+    try {
+      const { data } = await axios.post('/api/login', { username, password })
+      commit('SET_USER', data)
+    } catch (error) {
+      if (error.response && error.response.status === 401) {
+        throw new Error('Bad credentials')
+      }
+      throw error
+    }
+  }
+}
