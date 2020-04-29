ebd172df2dea7ef104fc080f8f961d2766b7be69
Mark Terence <mtiglao.ccheetah@gmail.com>
2020-04-29 10:16:28 +0800

    chore: remove un-used floating-input css

--- components/Login.vue
+++ components/Login.vue
@@ -50,45 +50,6 @@ export default {
 </script>
 
 <style module>
-.floatingLabel {
-  font-style: italic;
-}
-
-.floating,
-.floatingLabel {
-  transition: all 0.2s;
-  touch-action: manipulation;
-}
-.floating::-webkit-input-placeholder {
-  opacity: 0;
-  transition: inherit;
-}
-.floating::input-placeholder {
-  opacity: 0;
-  transition: inherit;
-}
-.floating:not(:placeholder-shown) + .floatingLabel,
-.floating:focus + .floatingLabel {
-  transform: translate(0.12rem, -0.2rem) scale(0.79);
-  top: 0;
-  position: absolute;
-}
-.floating:not(:placeholder-shown) + .floatingLabel {
-  color: #364fc7 !important;
-  font-weight: 800;
-  font-style: normal;
-}
-.floating:placeholder-shown + .floatingLabel {
-  cursor: text;
-  max-width: 66.66%;
-  white-space: nowrap;
-  overflow: hidden;
-  text-overflow: ellipsis;
-  transform-origin: left bottom;
-  transform: translate(0.8rem, 0.899rem) scale(1.2);
-  top: 0;
-  position: absolute;
-}
 .container {
   min-height: 100vh;
   display: flex;
