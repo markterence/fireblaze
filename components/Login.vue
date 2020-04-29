b52a5ca1f042200f0b1c21382823e265209c3394
Mark Terence <mtiglao.ccheetah@gmail.com>
2020-04-29 12:23:34 +0800

    chore: move Login component on LoginPage and use index.vue as app home page

--- components/Login.vue
+++ components/Login.vue
@@ -1,6 +1,7 @@
 <template>
   <div :class="[$style.container, $style.loginContainer]">
     <b-container>
+      <p v-if="formError">{{ formError }}</p>
       <b-row class="mb-2 justify-content-center">
         <b-col sm="8" md="6" lg="4">
           <FloatingInput
@@ -18,14 +19,19 @@
             id="password"
             v-model="secret"
             autocomplete="off"
-            type="text"
+            type="password"
             label="Secret Key"
           />
         </b-col>
       </b-row>
       <b-row class="mt-4 mb-2 justify-content-center">
         <b-col sm="8" md="6" lg="4">
-          <b-button :class="[$style.loginBtn]" variant="dark" block>
+          <b-button
+            :class="[$style.loginBtn]"
+            variant="dark"
+            block
+            @click="login"
+          >
             Login
           </b-button>
         </b-col>
@@ -43,7 +49,23 @@ export default {
   data() {
     return {
       accessKey: null,
-      secret: null
+      secret: null,
+      formError: null
+    }
+  },
+  methods: {
+    async login() {
+      try {
+        await this.$store.dispatch('auth/login', {
+          username: this.accessKey,
+          password: this.secret
+        })
+        this.accessKey = ''
+        this.secret = ''
+        this.formError = null
+      } catch (e) {
+        this.formError = e.message
+      }
     }
   }
 }
