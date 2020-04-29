47351017de190f0384fd8fc46eb09619d0e4f3e4
Mark Terence <mtiglao.ccheetah@gmail.com>
2020-04-29 19:53:11 +0800

    design: move search component on sidebar header to prevent it from scrolling
    * Add cosmtics

--- pages/index.vue
+++ pages/index.vue
@@ -5,41 +5,56 @@
       z-index="1"
       :visible="true"
       no-header-close
-      shadow
       bg-variant="dark"
       text-variant="light"
       no-close-on-route-change
       width="240px"
     >
       <template v-slot:title>
-        <div>
-          Firebase
+        <div class="shadow">
+          <div>
+            Fireblaze 🔥🔥🔥
+          </div>
+
+          <div class="mt-4">
+            <div class="d-flex justify-content-end align-items-center">
+              <b-input
+                size="sm"
+                placeholder="Filter by volume name..."
+                type="text"
+                class="rounded-pill mb-1"
+              />
+              <SearchIcon
+                class="text-dark position-absolute mr-1 mb-1"
+                style="right: 28px;"
+                width="18px"
+              />
+            </div>
+          </div>
         </div>
       </template>
       <!-- content -->
-      <div class="mt-4">
-        <div class="shadow border-bottom">
-          <div class="px-2">
-            <b-input
-              size="sm"
-              placeholder="Filter by name..."
-              type="text"
-              class="rounded-pill mb-1"
-            ></b-input>
-          </div>
-        </div>
+      <div>
         <b-nav vertical class="my-1">
-          <b-nav-item link-classes="text-light" :to="'/volume/2'">
-            Active
+          <b-nav-item
+            v-for="volume in 30"
+            :key="`volume-${volume}`"
+            :exact-active-class="$style.sidebarLinkActive"
+            :link-classes="[$style.sidebarLink, 'text-light']"
+            :to="'/volume/' + volume"
+          >
+            volume #{{ volume }}
           </b-nav-item>
-          <b-nav-item link-classes="text-light">Link</b-nav-item>
-          <b-nav-item link-classes="text-light">Another Link</b-nav-item>
-          <b-nav-item link-classes="text-light" disabled>Disabled</b-nav-item>
+          <b-nav-item
+            :link-classes="[$style.sidebarLink, 'text-light']"
+            disabled
+            >Disabled</b-nav-item
+          >
         </b-nav>
       </div>
       <template v-slot:footer>
-        <div>
-          Something Cool !
+        <div class="p-2">
+          Something 🆒 👍!
         </div>
       </template>
     </b-sidebar>
@@ -50,17 +65,17 @@
           <b-nav-item to="/">Home</b-nav-item>
         </b-navbar-nav>
         <b-navbar-nav class="ml-auto">
-          <b-nav-item-dropdown right no-caret>
+          <b-nav-item-dropdown href="" right no-caret>
             <template v-slot:button-content>
               <b-avatar size="36px"></b-avatar>
             </template>
-            <b-dropdown-item class="p-0 p-1" link-class="p-0 pl-1"
-              >Administration</b-dropdown-item
+            <b-dropdown-item-button
+              class="p-0 p-1"
+              link-class="p-0 pl-1"
+              @click="logout"
             >
-            <b-dropdown-divider></b-dropdown-divider>
-            <b-dropdown-item class="p-0 p-1" link-class="p-0 pl-1"
-              >Sign Out</b-dropdown-item
-            >
+              Sign Out
+            </b-dropdown-item-button>
           </b-nav-item-dropdown>
         </b-navbar-nav>
       </b-navbar>
@@ -73,13 +88,30 @@
   </div>
 </template>
 <script>
+import SearchIcon from '~/components/SearchIcon.vue'
 export default {
-  middleware: 'authenticated'
+  middleware: 'authenticated',
+  components: {
+    SearchIcon
+  },
+  methods: {
+    logout() {
+      this.$store.dispatch('auth/logout')
+      this.$router.push('/login')
+    }
+  }
 }
 </script>
 
 <style module>
 .content {
   margin-left: 240px;
+}
+
+.sidebarLink:hover {
+  background-color: hsl(210, 10%, 18%);
+}
+.sidebarLinkActive {
+  background-color: hsl(210, 10%, 15%);
 }
 </style>
